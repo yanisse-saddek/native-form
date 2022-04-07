7db77b4915b44c353351b0df7b360ed746730181
yanisse-saddek <yanisse321@gmail.com>
2022-04-07 12:48:31 +0200

    first commit'

--- App.js
+++ App.js
@@ -1,15 +1,49 @@
-import { View, Text } from "react-native";
-
+import { View, Text, TextInput, Switch, Slider, StyleSheet } from "react-native";
+import {useState} from 'react'
 export default function App() {
+  const [email, setEmail] = useState(0)
+  const [password, setPassword] = useState(0)
+
+
+  const changeMail = (e)=>{
+      setEmail(e)
+  }
+  const changePass = (e)=>{
+        setPassword(e)
+  }
+  
+  const styles = StyleSheet.create({
+    input : {
+      border:`1px solid  ${email.length>8?"green":"red"}`,
+      width:300,
+      height:40,
+      borderRadius:30,
+    },
+    inputPass : {
+      border:`1px solid ${password.length>8?"green":"red"}`,
+      width:300,
+      height:40,
+      borderRadius:30,
+    }
+  })
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
+      }}>
+        <View>
+            <Text>Email</Text>
+            <TextInput onChangeText={(e)=>{
+              changeMail(e)
+            }}editable style={styles.input}/>
+
+          <Text>Mot de passe</Text>
+          <TextInput secureTextEntry={true} onChangeText={(e)=>{
+              changePass(e)
+            }}editable style={styles.inputPass}/>
+        </View>
     </View>
   );
 }
